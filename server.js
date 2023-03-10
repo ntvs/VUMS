bb4229c03bba38ab64e2720265ca35dafec6df2f
ntvs <NickRibs@hotmail.com>
2023-03-10 11:43:03 -0500

    Updated syntax

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 //Express setup
-const express = require('express')
-const app = express()
-const port = 2080
+const express = require('express');
+const app = express();
+const port = 2080;
 
 //Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 //Server listening behavior
 app.listen(port, () => {
-  console.log(`\n[Server] now listening on ======> http://localhost:${port}\n`)
-})
+  console.log(`\n[Server] now listening on ======> http://localhost:${port}\n`);
+});
