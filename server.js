4a3751f0f43f7d31ea0a0b2561bf440307c32197
ntvs <NickRibs@hotmail.com>
2023-03-14 13:47:27 -0400

    Correct server.js style

--- server.js
+++ server.js
@@ -13,41 +13,41 @@ const { upload, fields, processUpload } = require('./uploader');
 
 //App uptime
 app.get('/', (req, res) => {
-  res.status(200).send({
-    "app": "VUMS",
-    "uptime": `${process.uptime()} s`
-  });
+    res.status(200).send({
+        "app": "VUMS",
+        "uptime": `${process.uptime()} s`
+    });
 });
 
 /*
     Notes:
-    incorrect empty file fields accepted, but incorrect file fields with a file throw an error 
-    incorrect text fields accepted, empty or not
+        incorrect empty file fields accepted, but incorrect file fields with a file throw an error 
+        incorrect text fields accepted, empty or not
 */
 
 //App primary function
 app.post('/', upload.fields(fields), (req, res) => {
 
-  console.log(req.body, req.files);
+    console.log(req.body, req.files);
 
-  if (req.body && req.files) {
-    res.status(200).send({
-      "body": req.body,
-      "files": req.files
-    });
-  } else if (req.body) {
-    res.status(200).send({
-      "body": req.body
-    });
-  } else {
-    res.status(400).send({
-      "msg": 'No fields submitted!',
-    });
-  }
+    if (req.body && req.files) {
+        res.status(200).send({
+            "body": req.body,
+            "files": req.files
+        });
+    } else if (req.body) {
+        res.status(200).send({
+            "body": req.body
+        });
+    } else {
+        res.status(400).send({
+            "msg": 'No fields submitted!',
+        });
+    }
 
 });
 
 //Server listening behavior
 app.listen(port, () => {
-  console.log(`\n[Dev Server] now listening on ======> http://localhost:${port}\n`);
+    console.log(`\n[Dev Server] now listening on ======> http://localhost:${port}\n`);
 });
